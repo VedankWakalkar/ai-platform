fea2dae11e47650d26962a5fa388db921bb42c2d
VedankWakalkar <131476793+VedankWakalkar@users.noreply.github.com>
2024-09-12 13:02:39 +0530

    Bootstrap

---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TransformationPage = () => {
+  return (
+    <div>
+      TransformationPage
+    </div>
+  )
+}
+
+export default TransformationPage
---
+++ app/(root)/transformations/[id]/update/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const UpdateTransformationPage = () => {
+  return (
+    <div>UpdateTransformationPage  
+    </div>
+  )
+}
+
+export default UpdateTransformationPage

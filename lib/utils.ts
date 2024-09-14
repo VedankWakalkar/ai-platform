e4b0c445c78f6c3206f7322d4179ea27cdf71db6
VedankWakalkar <131476793+VedankWakalkar@users.noreply.github.com>
2024-09-14 19:03:39 +0530

    Fix line endings

--- lib/utils.ts
+++ lib/utils.ts
@@ -91,7 +91,7 @@ export const debounce = (func: (...args: any[]) => void, delay: number) => {
   let timeoutId: NodeJS.Timeout | null;
   return (...args: any[]) => {
     if (timeoutId) clearTimeout(timeoutId);
-    timeoutId = setTimeout(() => func.apply(null, args), delay);
+    timeoutId = setTimeout(() => func(...args), delay);
   };
 };
 
